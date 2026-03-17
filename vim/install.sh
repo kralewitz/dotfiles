84c9b4c70513d366ecb9175eb1e00767d3031cab
Jan Kral <jan.kral@4net.tv>
2026-03-17 09:03:32 +0100

    Update dotfiles

--- vim/install.sh
+++ vim/install.sh
@@ -5,5 +5,15 @@ set -o errexit -o nounset
 # Create vim directory structure
 if [ ! -d ~/.vim ]; then
 	mkdir -p ~/.vim/tmp
+        mkdir -p ~/.vim/pack/plugins/start
+fi
+
+if [ ! -d ~/.vim/pack/plugins/start/vim-toml ]; then
+        git clone https://github.com/cespare/vim-toml.git ~/.vim/pack/plugins/start/vim-toml
+else
+        echo Updating vim-toml plugin
+        pushd ~/.vim/pack/plugins/start/vim-toml
+        git pull
+        popd
 fi
 
